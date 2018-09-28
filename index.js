7ae28bbeb92a594ce7a08eee49a74e948ae8620e
alpha-class-7596 <rajwindersmunder@gmail.com>
2018-09-28 03:35:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+
+
+function sayHelloToRaj(){
+  console.log("hello , rj")
+}
+function sayHelloToGagan(){
+  console.log("hello gagan")
+}
+function sayHelloToNavi(){
+  console.log("hello ")
+}
+
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`)
+}
+ 
+sayHelloTo('Isabel') // "Hello, Isabel!"
+sayHelloTo("Jane") // "Hello, Jane!"
+sayHelloTo("R2-D2") // "Hello, R2-D2!"
+sayHelloTo(1)
