666671e5524764adb1ac6a240aefd4a2422fc6d8
alpha-class-7596 <rajwindersmunder@gmail.com>
2018-09-28 03:35:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHelloToNavi(){
 function sayHelloTo(firstName) {
   console.log(`Hello, ${firstName}!`)
 }
- 
+
 sayHelloTo('Isabel') // "Hello, Isabel!"
 sayHelloTo("Jane") // "Hello, Jane!"
 sayHelloTo("R2-D2") // "Hello, R2-D2!"
